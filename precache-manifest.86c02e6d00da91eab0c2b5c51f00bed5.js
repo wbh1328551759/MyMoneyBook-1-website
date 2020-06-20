339e2d14e372bb2b5014f08ade7c6807479a06c1
wbh1328551759 <1328551759@qq.com>
2020-06-21 02:18:39 +0800

    deploy

---
+++ precache-manifest.86c02e6d00da91eab0c2b5c51f00bed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed8ba047b8e3335c430",
+    "url": "/MyMoneyBook-1-website/css/app.75836ba9.css"
+  },
+  {
+    "revision": "c22a0c887b7e740a4d5658d21c6481d8",
+    "url": "/MyMoneyBook-1-website/index.html"
+  },
+  {
+    "revision": "4ed8ba047b8e3335c430",
+    "url": "/MyMoneyBook-1-website/js/app.acd60f7b.js"
+  },
+  {
+    "revision": "f9f197eb5c3cda1d03e9",
+    "url": "/MyMoneyBook-1-website/js/chunk-vendors.4b4b2f3b.js"
+  },
+  {
+    "revision": "7d81168cd02d66004bfc0ce7e1e07f7b",
+    "url": "/MyMoneyBook-1-website/manifest.json"
+  },
+  {
+    "revision": "f8760d8907f4962425fea5ca966b5fc6",
+    "url": "/MyMoneyBook-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MyMoneyBook-1-website/robots.txt"
+  }
+]);
